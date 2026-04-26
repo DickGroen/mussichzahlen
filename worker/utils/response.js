ea181286a09ff3ad832bbf747cf862cce9b1e8b2
DickGroen <dickgroen2@gmail.com>
2026-04-26 20:57:04 +0200

    Add CORS headers and response utility functions

--- worker/utils/response.js
+++ worker/utils/response.js
@@ -1,1 +1,17 @@
 
+const CORS_HEADERS = {
+  "Access-Control-Allow-Origin":  "*",
+  "Access-Control-Allow-Methods": "POST, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type"
+};
+
+export function jsonResponse(data, status = 200) {
+  return new Response(JSON.stringify(data), {
+    status,
+    headers: { "Content-Type": "application/json", ...CORS_HEADERS }
+  });
+}
+
+export function corsResponse() {
+  return new Response(null, { headers: CORS_HEADERS });
+}
