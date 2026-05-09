e89dda190a610c75376972c27ba1b9d73247fce5
DickGroen <dickgroen2@gmail.com>
2026-05-09 11:24:30 +0200

    Enhance CORS headers and jsonResponse formatting
    Updated CORS headers to include 'Authorization' and 'Access-Control-Max-Age'. Modified jsonResponse to format JSON output and added charset to Content-Type.

--- worker/utils/response.js
+++ worker/utils/response.js
@@ -1,17 +1,25 @@
+// worker/utils/response.js
 
 const CORS_HEADERS = {
-  "Access-Control-Allow-Origin":  "*",
-  "Access-Control-Allow-Methods": "POST, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type"
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  "Access-Control-Max-Age": "86400",
 };
 
 export function jsonResponse(data, status = 200) {
-  return new Response(JSON.stringify(data), {
+  return new Response(JSON.stringify(data, null, 2), {
     status,
-    headers: { "Content-Type": "application/json", ...CORS_HEADERS }
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      ...CORS_HEADERS,
+    },
   });
 }
 
 export function corsResponse() {
-  return new Response(null, { headers: CORS_HEADERS });
+  return new Response(null, {
+    status: 204,
+    headers: CORS_HEADERS,
+  });
 }
